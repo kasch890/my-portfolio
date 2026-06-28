1cbf7b8a16d84b1d7ba5ad548119668204bef639
kasch890 <kai.fernandes890@gmail.com>
2026-06-28 12:53:41 -0400

    Home.jsx component made

---
+++ src/sections/Home.jsx
@@ -0,0 +1,11 @@
+// sections/Home.jsx
+function Home() {
+  return (
+    <main>
+      <h1>Kai Fernandes</h1>
+      <p>Software Developer | UX Designer | MS Software Development</p>
+    </main>
+  );
+}
+
+export default Home;
