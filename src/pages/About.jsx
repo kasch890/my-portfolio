8cf561288e641410c592108deecf325442d81163
kasch890 <kai.fernandes890@gmail.com>
2026-06-28 11:31:59 -0400

    Added basic structre for each page

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+// src/pages/About.jsx
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I'm Kai Fernandes, a software developer focused on full-stack development,
+        UX design, and data-driven applications.
+      </p>
+    </section>
+  );
+}
+
+export default About;
