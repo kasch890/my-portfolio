9dd7e3c63ebbc59936ad97316abb693fa1ecd911
kasch890 <kai.fernandes890@gmail.com>
2026-06-28 13:32:57 -0400

    Added basic information in about and to a couple of projects. incorporated github links that open in new tabs

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,10 +4,11 @@ function About() {
     <section id="about">
       <h2>About Me</h2>
       <p>
-        I'm Kai Fernandes, a software developer focused on full-stack development,
-        UX design, and data-driven applications.
-
-        TESTING CHANGES FOR DEPLOYMENT
+        I'm Kai Fernandes, an aspiring software developer and engineer with a passion for creating innovative solutions. I have a strong foundation in programming languages such as Python, Java, JavaScript, and HTML/CSS, and I enjoy exploring new technologies and frameworks. My goal is to contribute to impactful projects that make a difference in people's lives.
+      </p>
+      <br></br>
+      <p>
+        I recieved my Bachelor's degree in Physics from Amherst College, where I developed strong analytical and problem-solving skills. I am currently pursuing a Master's degree at Boston University with a graduate certificate in Software Engineering. I am eager to apply my knowledge and skills in a professional setting, and I am open to opportunities that allow me to grow as a developer and make meaningful contributions to the tech industry.
       </p>
     </section>
   );
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,6 +2,17 @@ function Projects() {
     return (
       <section id="projects">
         <h2>Projects</h2>
+        <h3>BariatricPath</h3>
+        <p><a href="https://github.com/BUMETCS673/cs673olsum26project-cs673olsum26team1" target="_blank" rel="noopener noreferrer">Github Repository</a></p>
+        <p>
+          Graduate coursework -- BariatricPath is a web application designed to assist patients in their bariatric surgery journey. It provides educational resources, progress tracking, and personalized recommendations to support patients before and after surgery. The application aims to improve patient outcomes and enhance the overall experience of bariatric surgery.
+        </p>
+        <br></br>
+        <h3>Personal Portfolio Website</h3>
+        <p><a href="https://github.com/kasch890/my-portfolio" target="_blank" rel="noopener noreferrer">Github Repository</a></p>
+        <p>
+          The website that you are currently looking at! This personal portfolio website is built using React and showcases my skills, projects, and experience as a software developer. It serves as a platform to present my work and connect with potential employers or collaborators.
+        </p>
       </section>
     );
   }
