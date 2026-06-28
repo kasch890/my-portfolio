92d7ad57717dfd7c99ad6497d5567032c6379f16
kasch890 <kai.fernandes890@gmail.com>
2026-06-28 18:56:58 -0400

    Minor structure change to projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,16 +2,22 @@ function Projects() {
     return (
       <section id="projects">
         <h2>Projects</h2>
+        <h3>Personal Portfolio Website</h3>
+        <p><a href="https://github.com/kasch890/my-portfolio" target="_blank" rel="noopener noreferrer">Github Repository</a></p>
+        <p>
+          The website that you are currently looking at! This personal portfolio website is built using React and showcases my skills, projects, and experience as a software developer. It serves as a platform to present my work and connect with potential employers or collaborators.
+        </p>
+        <br></br>
         <h3>BariatricPath</h3>
         <p><a href="https://github.com/BUMETCS673/cs673olsum26project-cs673olsum26team1" target="_blank" rel="noopener noreferrer">Github Repository</a></p>
         <p>
           Graduate coursework -- BariatricPath is a web application designed to assist patients in their bariatric surgery journey. It provides educational resources, progress tracking, and personalized recommendations to support patients before and after surgery. The application aims to improve patient outcomes and enhance the overall experience of bariatric surgery.
         </p>
         <br></br>
-        <h3>Personal Portfolio Website</h3>
-        <p><a href="https://github.com/kasch890/my-portfolio" target="_blank" rel="noopener noreferrer">Github Repository</a></p>
+        <h3>Survey Data Analysis Pipeline</h3>
+        <p><a href="" target="_blank" rel="noopener noreferrer">GitHub Repository</a></p>
         <p>
-          The website that you are currently looking at! This personal portfolio website is built using React and showcases my skills, projects, and experience as a software developer. It serves as a platform to present my work and connect with potential employers or collaborators.
+        Automated survey metrics analysis using Python (Pandas, NumPy), reducing processing time by ~98% (2 hours → 2 minutes). Improved reporting clarity by standardizing data transformations and output structures.
         </p>
       </section>
     );
