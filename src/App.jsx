520d0ba5a955141891142ca1a178e36afd1dbc47
kasch890 <kai.fernandes890@gmail.com>
2026-06-28 22:06:40 -0400

    Page styling now includes max width for sections while allowing navbar to fill screen width

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,25 @@ import About from "./components/AboutSection";
 import Projects from "./components/ProjectsSection";
 import Skills from "./components/SkillsSection";
 import Contact from "./components/ContactSection";
+import "./App.css";
 
 function App() {
   return (
     <>
     <Navbar />
-      <section id="home">
+      <section id="home" className="page-section">
         <Home />
       </section>
-      <section id="about">
+      <section id="about" className="page-section">
         <About />
       </section>
-      <section id="projects">
+      <section id="projects" className="page-section">
         <Projects />
       </section>
-      <section id="skills">
+      <section id="skills" className="page-section">
         <Skills />
       </section>
-      <section id="contact">
+      <section id="contact" className="page-section">
         <Contact />
       </section>
     </>
